b14eb41252450b0e8ae10d1c602b23bcaffdf350
Alex Eagle <alex@aspect.dev>
2024-10-04 07:39:00 -0700

    fix: should run outside bazel too

--- say.py
+++ say.py
@@ -3,7 +3,9 @@ from os import path, environ, sep
 
 # Note, we could use https://pypi.org/project/bazel-runfiles/
 # for a more robust lookup mechanism of files provided in the `data` attribute.
-FOLDER=path.dirname(__file__).split(environ['BAZEL_WORKSPACE'] + sep)[-1]
+WORKSPACE=getenv('BAZEL_WORKSPACE', '')
+FOLDER=path.dirname(__file__).split(WORKSPACE + sep)[-1]
+
 
 def moo(text):
     cowsay.cow(text)
